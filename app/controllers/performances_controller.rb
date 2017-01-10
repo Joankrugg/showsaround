41d9c08fe37bc4955efff0a261a0da8ebb7cd0db
Philippe chambon <joan_krugg@live.fr>
2017-01-10 23:10:06 +0100

    devise

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,12 +1,19 @@
 class PerformancesController < ApplicationController
+
+  before_action :set_performance, only: [:show, :edit, :update, :destroy]
+
   def index
     @performances = Performance.all
   end
 
   def new
+    @performance = Performance.new
   end
 
   def create
+    @performance = Performance.new(performance_params)
+    @performance.save
+    redirect_to performance_path(@performance)
   end
 
   def show
@@ -16,8 +23,23 @@ class PerformancesController < ApplicationController
   end
 
   def update
+    @performance.update(performance_params)
+    redirect_to performance_path(@performance)
   end
 
   def destroy
+    @performance.destroy
+    redirect_to performances_path
+  end
+
+  private
+
+
+  def set_performance
+    @performance = Performance.find(params[:id])
+  end
+
+  def performance_params
+    params.require(:performance).permit(:perf_price, :facebook_event)
   end
 end
